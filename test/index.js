c6ccdd1ef2dc58e0b4a3cf5b73fa0df7f35c5a6f
Henry Snopek <hhsnopek@gmail.com>
2015-09-23 18:53:56 -0500

    add tests

--- test/index.js
+++ test/index.js
@@ -1,36 +1,29 @@
-var fs = require('fs');
+var fs = require('co-fs');
 var path = require('path');
 var should = require('chai').should();
 var Duo = require('duo');
 var duoPostcss = require('../index.js');
 
-var _fixture = function(file) {
-  return function(file) {
-    this.file = `test/fixtures/${file}.css`;
-    var duo = Duo(__dirname);
-    duo.entry(this.file);
-    duo.use(duoPostcss());
-    return duo.run().code;
-  }
-}
-
-var _expected = function(file) {
-  this.file = `test/expected/${file}.css`;
-  return fs.readFileSync(this.file, 'utf-8');
-}
-
 describe('duo-postcss', function() {
-  console.log(duoPostcss);
-  it('should render basic css', function () {
-    this.fixture = yield _fixture('basic', 'basic');
-    this.expected = _expected('basic', 'basic');
-    console.log("almost done");
-    this.fixture.should.equal(this.expected);
+  it('should render basic css', function *() {
+    var duo = Duo(__dirname);
+    duo.entry(join(__dirname, "test/fixtures/basic.css"));
+    duo.use(duoPostcss);
+    this.fixture = (yield duo.run()).code;
+    this.expected =
+      yield fs.readFile(join(__dirname, "test/expected/basic.css"));
+
+    this.fixture.should.equal(expected);
   });
 
-  it('should render css with a plugin', function () {
-    this.fixture = yield _fixture('plugin', 'plugin');
-    this.expected = yield _expected('plugin', 'plugin');
+  it('should render css with a plugin', function *() {
+    var duo = Duo(__dirname);
+    duo.entry(join(__dirnam, "test/fixtures/plugin.css"));
+    duo.use(duoPostcss);
+    this.fixture = (yield duo.run()).code;
+    this.expected =
+      yield fs.readFile(join(__dirname, "test/expected/plugin.css"));
+
     this.fixture.should.equal(this.expected);
   });
 });
